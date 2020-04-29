5cca8eb79c20630ef44ed03298a50322bf846300
Serjio2888 <kerchtown@yandex.ru>
2020-04-29 14:22:49 +0300

    ports

--- internal/pkg/utils/rec.go
+++ internal/pkg/utils/rec.go
@@ -8,7 +8,7 @@ import (
 
 func Rec(qName string) []byte {
 	// подключение и объявление очереди вынести в отдельную функцию и не вызывать каждый раз
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:15672/")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
